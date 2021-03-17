c08e116c39eef6ebda54dea8c0f84b4678a5905d
Michael Huber <huber.michael@virology.uzh.ch>
2021-03-17 19:16:17 +0100

    import multiple xls, update master and blocklist

--- Statistik.R
+++ Statistik.R
@@ -4,36 +4,46 @@ library(readxl)
 library(cowplot)
 library(shinythemes)
 library(lubridate)
+library(janitor)
 
+master = read.csv("/Users/huber.michael/Dropbox/Repositories/DIA_Stats/master_table.csv")
+blocklist = read.csv("/Users/huber.michael/Dropbox/Repositories/DIA_Stats/blocklist.csv") %>% pull(Code)
+path = "/Users/huber.michael/Dropbox/Repositories/DIA_Stats/Data/"
 
-master = read.csv("~/Documents/R-Statistiken/Beispiel R Michael/master_table.csv")
-raw_data = read_excel("~/Documents/R-Statistiken/Beispiel R Michael/test_export.xlsx")
+export_raw =
+    list.files(path, pattern = "*.xls*", full.names = TRUE) %>% 
+    map_df(~read_excel(.)) %>%
+    clean_names(case = "parsed")
 
-
-data = raw_data %>%
+data =
+    export_raw %>%
+    filter(!is.na(Anforderungsnr),
+           !is.na(Patientennummer),
+           !is.na(RES)) %>%
+    # only keep one entry per test and patient sample
+    unique() %>%
     select(Einsender, MC, Eingangsdatum) %>%
     mutate(Eingangsdatum = as.Date(Eingangsdatum, "%d.%m.%Y")) %>%
+    # filter(Eingangsdatum >= input$date[1] & Eingangsdatum <= input$date[2]) %>%
+    select(-Eingangsdatum) %>%
     rename(Code = MC) %>%
     left_join(. , master) %>%
-    filter(!(is.na(Analyse))) %>%
+    #filter(!(is.na(Analyse))) %>%
+    
+    ### remove MCs (MATD, cohort)
+    filter(!Code %in% blocklist) %>%
     
     ### assign new code to anonymous HIV tests
     mutate(Code = ifelse(Einsender == "HIVANO" & Code == "H06USD", "XXX", Code)) %>%
     mutate(Code = ifelse(Einsender == "HIVANO" & Code == "H06UST", "XXX", Code)) %>%
     
-    ### assign new code to SARS PCR C23F*
-    mutate(Code = ifelse(Code == "C23FKP", "C23FK", Code)) %>%
-    mutate(Code = ifelse(Code == "C23FPU", "C23FK", Code)) #%>%
-
-    ### filter out SARS
-    #filter(!Code %in% c("C23FK", "C23RS", "C23PS"))
-
-data_filtered = data %>%
-#     filter(Eingangsdatum >= input$date[1] & Eingangsdatum <= input$date[2]) %>%
-     select(-Eingangsdatum)
+    ### assign new code to SARS PCR C23F/G*
+    mutate(Code = ifelse(Code %in% c("C23GK", "C23GKA", "C23GKU", "C23FKP", "C23FPU"), "C23FK", Code)) 
 
 
-Bereich = data_filtered %>%
+#%>% filter(is.na(Analyse)) %>% pull(Code) %>% unique()
+
+Bereich = data %>%
     mutate(type = paste(Arbeitsbereich, Unterbereich, Weitere_Unterteilung)) %>%
     group_by(type) %>%
     mutate(Subtotal = n()) %>%
@@ -43,18 +53,18 @@ Bereich = data_filtered %>%
     ungroup() %>%
     mutate(Gesamttotal = sum(Subtotal)) %>%
     select(-Einsender, -type, -Code, -Pathogen, -Analyse)
+write_excel_csv(Bereich, paste0(path, "Bereich.csv"))
 
-
-Verfahren = data_filtered %>%
+Verfahren = data %>%
         group_by(Code) %>%
         mutate(Total = n()) %>%
         sample_n(1) %>%
         ungroup() %>%
         arrange(Pathogen) %>%
         select(Pathogen, Analyse, Total)
+write_excel_csv(Verfahren, paste0(path, "Verfahren.csv"))
     
-    
-Plot = Bereich %>%
+plot = Bereich %>%
             mutate(Arbeitsbereich = case_when(
                 Arbeitsbereich == "PCR-basierte Analytik" ~ "PCR",
                 Arbeitsbereich == "Sequenzbasierte Analytik" ~ "Sequenz",
@@ -70,4 +80,5 @@ Plot = Bereich %>%
             coord_flip() +
             theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
 
-Plot
+plot
+ggsave(paste0(path, "Plot.jpeg"), plot)
